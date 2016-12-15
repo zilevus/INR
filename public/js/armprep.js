117456d92576479ed75ad42cfe1619a8049e4236
Ethan <ephan@vt.edu>
2016-12-15 09:44:25 -0500

    Another check

--- public/js/armprep.js
+++ public/js/armprep.js
@@ -1,29 +1,25 @@
 var socket = io();
-var cont = false;
-$(window).load(function() {
-  socket.on('message',function(data){
-      console.log("HERE");
-      var html="";
-      if(data.message!=null){
-          var ready=JSON.parse(data.message);
-          console.log(ready.error1);
-          if (ready.error1 == "1") {
-              cont = true;
-          } else if (ready.error1 == 1) {
-            cont = true;
-          }
-          //console.log(obj);
-          //alert(scoreresponse);
-      }else{
-          console.log("Problem",data);
-      }
-
-  });
-});
-
 function sendresponse() {
+    var cont = false;
     var loopNum = sessionStorage.getItem("loopNum");
     var exerciseNum = sessionStorage.getItem("exerciseNum");
+    socket.on('message',function(data){
+        console.log("HERE");
+        var html="";
+        if(data.message!=null){
+            var ready=JSON.parse(data.message);
+            if (ready.error1 == "1") {
+                cont = true;
+            } else if (ready.error1 == 1) {
+              cont = true;
+            }
+            //console.log(obj);
+            //alert(scoreresponse);
+        }else{
+            console.log("Problem",data);
+        }
+
+    });
     if (cont) {
       var systemReady="{\"type\" : \"SystemReady\",\"task\" : " +exerciseNum.toString()+"}";
       socket.emit("json", systemReady);
